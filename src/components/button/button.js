94d8cb4345da3ee9b2b283fc4f584ca68d3d385e
aaa-mariah-weathersby <weathersby.mariah@aaa-calif.com>
2018-07-30 13:30:37 -0700

    Initialize new design repo

---
+++ src/components/button/button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ButtonBase from '@material-ui/core/Button';
+
+const Button = (props) => {
+
+  // return ( 
+  // )
+
+}
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired
+};
+
+export default Button;
