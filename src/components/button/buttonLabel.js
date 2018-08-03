201061cddd8818ab89bc592438f73adf0d9163fd
aaa-mariah-weathersby <weathersby.mariah@aaa-calif.com>
2018-08-03 09:38:05 -0700

    override mui styles

--- src/components/button/buttonLabel.js
+++ src/components/button/buttonLabel.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './styles/buttonLabel/buttonLabel.css'
+import styles from './styles/buttonLabel/buttonLabel.scss'
+
+console.log("label styles: ", styles )
 
 const ButtonLabel = (props) => {
     return (
-        <h4 className={"button_label"}>
+        <h4 className={styles.button_label}>
             {props.label}
         </h4>
     )
--- src/components/button/primaryButton.js
+++ src/components/button/primaryButton.js
@@ -3,34 +3,50 @@ import PropTypes from 'prop-types';
 import ButtonBase from '@material-ui/core/Button';
 import ButtonLabel from './buttonLabel';
 
-import './styles/primary/primaryButton.css'
+// import './styles/primary/primaryButton.css'
+import styles from './styles/primary/primaryButton.scss'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 
 class PrimaryButton extends Component {
+
 
   state = {
 
   };
 
-  render = () => {
-    return ( 
-      <ButtonBase 
-        onClick = {this.props.onClick}
-
-        disableRipple = {true}
-        disableTouchRipple = {true}
-        focusRipple = {false}
-
-        classes = {{
-          root: 'primary_button button'
-        }}
-
-        children = { 
-          <ButtonLabel
-            label={this.props.label}
-          />
+  themeOverride = createMuiTheme({
+    overrides: {
+      MuiButtonBase: {
+        root: {
+          backgroundColor: 'rgba(0, 153, 255, 1)',
         }
-      />
+      }
+    }
+  })
+
+  render = () => {
+
+    return ( 
+      <MuiThemeProvider theme={this.themeOverride}>
+        <ButtonBase 
+          onClick = {this.props.onClick}
+
+          disableRipple = {true}
+          disableTouchRipple = {true}
+          focusRipple = {false}
+
+          classes = {{
+            root: styles.primary_button
+          }}
+
+          children = { 
+            <ButtonLabel
+              label={this.props.label}
+            />
+          }
+        />
+      </MuiThemeProvider>
     )
   }
 
